4a21b44a7dda6af3c424a4721662d3f2423e01e3
David Grammatico <david.lugdunum@gmail.com>
2022-01-15 16:39:59 +0100

    add session ui

--- src/Chrono/Chrono.js
+++ src/Chrono/Chrono.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "./Chrono.css"
+import PauseImg from "../Images/pause.svg"
+import PlayImg from "../Images/play.svg"
+import ResetImg from "../Images/reset.svg"
 
 export default function Chrono() {
-  return (
-    <div>
 
+  const [sessionTime, setSessionTime] = useState(1500)
+  const [sessionTimeFixed, setSessionTimeFixed] = useState(1500)
+  const [breakTime, setBreakTime] = useState(300)
+  const [breakTimeFixed, setBreakTimeFixed] = useState(300)
+  const [workingChrono, setWorkingChrono] = useState(false)
+
+  return (
+    <div className="container-chrono">
+      <div className="container-config">
+        <div className="box-btns session">
+          <button className='minus'>-</button>
+          <span>{sessionTimeFixed / 60}</span>
+          <button className="plus">+</button>
+        </div>
+      </div>
     </div>
   )
 }
